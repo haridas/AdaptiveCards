76995c2adb49ece4a7e5d56c29d78bac7d95e563
amitr <amit.ramachandran@imaginea.com>
2020-08-13 12:47:24 +0530

    Updated font size threshold using clustering

--- source/pic2card/mystique/default_host_configs.py
+++ source/pic2card/mystique/default_host_configs.py
@@ -1,6 +1,22 @@
 IMAGE_SIZE = {
-        80: "Small",
-        120: "Medium",
-        180: "Large",
-        500: "Auto"
+    80: "Small",
+    120: "Medium",
+    180: "Large",
+    500: "Auto"
+}
+
+# Font-size and weight thresholds
+FONT_SIZE = {
+    "small": "0.02734",
+    "default": "0.02872",
+    "medium": "0.03634",
+    "large": "0.03868",
+    "extralarge": "0.05463"
+}
+
+FONT_WEIGHT = {
+    "small": "5",
+    "medium": "6.5",
+    "large": "8",
+    "extralarge": "9"
 }
